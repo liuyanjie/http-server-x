0e38fee775b35a28a0428b5321318ab3a697f8ae
liuyanjie <lyj8888888888@gmail.com>
2016-04-28 13:42:51 +0800

    调整示例

--- example/server.js
+++ example/server.js
@@ -2,42 +2,45 @@
 
 const HttpServer = require('../lib/index');
 
-let httpServer = new HttpServer({});
+let httpServer = new HttpServer({logging: true});
 
 let router = HttpServer.Router();
 
 function fn1(req, res, next) {
-  console.log('--------------fn1');
-  setTimeout(() => {
-    this.fn1 = 'pass';
-    this.body = {};
-    next();
-  }, 1);
+  console.log('--------------fn1', typeof next);
+  next();
 }
 
-function fn2() {
+function fn2(req, res) {
   console.log('--------------fn2');
-  this.body.fn2 = 'pass';
+  //ctx.body.fn2 = 'pass';
+  res.json({v: 'fn2'});
+  //return Promise.resolve();
+}
+
+function fn3(ctx) {
+  console.log('--------------fn3');
+  ctx.body.fn2 = 'pass';
   return Promise.resolve();
 }
 
-function* gfn1() {
+function* gfn1(ctx) {
   console.log('--------------gfn1');
-  this.body.gfn1 = 'pass';
+  ctx.body.gfn1 = 'pass';
   var result = yield Promise.resolve(true);
   return result;
 }
 
-function* gfn2() {
+function* gfn2(ctx) {
   console.log('--------------gfn2');
-  this.body.gfn2 = 'pass';
+  ctx.body.gfn2 = 'pass';
   var result = yield Promise.resolve(true);
   return result;
 }
 
-function fn3() {
+function fn3(ctx) {
   console.log('--------------fn3');
-  this.body.fn3 = 'pass';
+  ctx.body.fn3 = 'pass';
 }
 
 function fn(req, res, next){
@@ -46,25 +49,9 @@ function fn(req, res, next){
 
 router.get('/fn', fn);
 
-router.get('/test/wares', HttpServer.wares([fn1, fn2, gfn1, gfn2, fn3]));
-
-function jsonData() {
-  this.body = {
-      firstName: 'Mohandas'
-    , lastName: 'Gandhi'
-    , aliases: [{
-          firstName: 'Mahatma'
-        , lastName: 'Gandhi'
-      }, {
-          firstName: 'Bapu'
-      }]
-  };
-}
-
-router.get('/test/wares/data.json', HttpServer.wares([jsonData]));
+router.get('/test/wares', HttpServer.wares([fn1, fn2, gfn1, gfn2]));
 
 httpServer.use(router);
 
-// httpServer.configure();
-
 httpServer.start();
+
